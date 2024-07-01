17a627f15709fd91092ca2086148190a67841d76
Eduardo Alencar <alencareduardo17@gmail.com>
2024-07-01 09:03:24 -0300

    Style dos elementos

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Eduu </Text>
+      
+      <Image
+      source={require('./src/crono.png')}
+      />
+
+      <Text style={styles.timer}> 00:00:00 </Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>INICIAR</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>LIMPAR</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}> Último tempo 00:03:10 </Text>
+      </View>
+
     </View>
   );
 }
@@ -13,6 +33,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#b0053c'
+  },
+  timer: {
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#b0053c'
+  },
+  areaUltima: {
+    marginTop: 40
+  },
+  textoCorrida: {
+    fontSize: 23,
+    color: '#FFF',
+    fontStyle: 'italic'
   }
 });
