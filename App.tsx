deedb16916bb31b10bdebdf758c9eec418b444a8
Eduardo Alencar <alencareduardo17@gmail.com>
2024-07-04 06:42:50 -0300

    Funcionalidades do cronômetro

--- App.tsx
+++ App.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
+let timer = null;
+let ss = 0;
+let mm = 0;
+let hh = 0;
+
 export default function App() {
+
+  const [numero, setNumero] = useState(0);
+  const [botao, setBotao] = useState('INICIAR');
+  const [ultimo, setUltimo] = useState(null);
+
+  function iniciar() {
+    if(timer != null) {
+      //Aqui vai parar o timer
+      clearInterval(timer);
+      timer = null;
+      
+      setBotao("INICIAR");
+    } else {
+      //Começar a girar o timer
+      timer = setInterval(() => {
+
+        ss++;
+
+        if(ss == 60) {
+          ss = 0;
+          mm++
+        }
+
+        if(mm == 60) {
+          mm = 0;
+          hh++;
+        }
+
+        let format = (hh < 10 ? '0' + hh : hh) + ':' 
+        + (mm < 10 ? '0' + mm : mm) + ':'
+        + (ss < 10 ? '0' + ss : ss);
+
+        setNumero(format);
+
+      }, 1000);
+      setBotao('PARAR');
+    }
+  }
+
+  function limpar() {
+    if(timer != null) {
+      //Parar o timer
+      clearInterval(timer);
+      timer = null
+    }
+
+    setUltimo(numero);
+
+    setNumero(0);
+    ss = 0;
+    mm = 0;
+    hh = 0;
+    setBotao('INICIAR');
+  }
+
   return (
     <View style={styles.container}>
       
@@ -9,20 +69,22 @@ export default function App() {
       source={require('./src/crono.png')}
       />
 
-      <Text style={styles.timer}> 00:00:00 </Text>
-
+      <Text style={styles.timer}> {numero} </Text>
+      
       <View style={styles.btnArea}>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnTexto}>INICIAR</Text>
+        <TouchableOpacity style={styles.btn} onPress={iniciar}>
+          <Text style={styles.btnTexto}> {botao} </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn}>
+        <TouchableOpacity style={styles.btn} onPress={limpar}>
           <Text style={styles.btnTexto}>LIMPAR</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.areaUltima}>
-        <Text style={styles.textoCorrida}> Último tempo 00:03:10 </Text>
+        <Text style={styles.textoCorrida}> 
+          { ultimo ? 'Ultimo tempo: ' + ultimo : '' }
+        </Text>
       </View>
 
     </View>
